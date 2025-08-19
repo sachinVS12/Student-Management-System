f7a9bc16c98fa8722d282727b0720952a1c96c38
Sachin <vs1sachi12@gmail.com>
2025-08-19 18:15:43 +0530

    first commit25

--- server/server.js
+++ server/server.js
@@ -38,6 +38,15 @@ app.post('/signup', async (req, res) => {
   await newUser.save()
   res.json({ message: 'User created' })
 });
+//signin
+app.post('/signin', async (req, res) => {
+  const { username, password}=req.body;
+  const user = await User.findOne({ username })
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid username or password' })
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password)
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
