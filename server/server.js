a51678111acb7d01ad730cdc1ce71582cd77b14b
Sachin <vs1sachi12@gmail.com>
2025-08-20 13:01:03 +0530

    first commit26

--- server/server.js
+++ server/server.js
@@ -27,26 +27,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something broke!' });
 });
 
-//signup
-app.post('/signup', async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username })
-  if (user) {
-    return res.status(400).json({ message: 'Username already exists' })
-  }
-  const newUser = new User({ username, password })
-  await newUser.save()
-  res.json({ message: 'User created' })
-});
-//signin
-app.post('/signin', async (req, res) => {
-  const { username, password}=req.body;
-  const user = await User.findOne({ username })
-  if (!user) {
-    return res.status(401).json({ message: 'Invalid username or password' })
-  }
-  const isPasswordValid = await bcrypt.compare(password, user.password)
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
