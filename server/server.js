80f370a9ff369a4b423f621d78b54416424baa6e
Sachin <vs1sachi12@gmail.com>
2025-08-21 18:07:43 +0530

    first commit31

--- server/server.js
+++ server/server.js
@@ -28,6 +28,9 @@ app.use((err, req, res, next) => {
 });
 
 
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
